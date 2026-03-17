9570f8503dfaa784714811146c77d4b1321ebe26
pistakral <michaloriesek7@gmail.com>
2026-03-17 16:07:59 +0100

    Updated Checkout.tsx

--- src/components/shop/Checkout.tsx
+++ src/components/shop/Checkout.tsx
@@ -411,6 +411,7 @@ export default function Checkout() {
                         setCodeError(null);
                       }}
                       className="flex-1 px-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent uppercase"
+                      placeholder="Zadajte kód"
                       disabled={discountApplied}
                     />
                     <button
